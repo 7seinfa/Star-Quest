2225602b685ccfa8f0028cb4969ccff9295fe8cf
Hussein Abdallah <habdal@uwo.ca>
2024-03-10 04:49:16 -0400

    my mainwindow

--- src/mainwindow.cpp
+++ src/mainwindow.cpp
@@ -1,4 +1,14 @@
+/**
+ * Author: Hussein Abdallah
+ * CS3307B
+ * This file is where the main window of the game exists.
+*/
+
+
+//header file for this
 #include "headers/mainwindow.h"
+
+//includes
 #include "headers/Background.h"
 #include <QTimer>
 #include <iostream>
@@ -10,11 +20,14 @@
 MainWindow::MainWindow(QWidget *parent)
   : QMainWindow(parent)
 {
-    srand(time(NULL));
+    srand(time(NULL)); //random seed for the random map later
+
+    //create background object
     QPixmap bg("img/background.png");
     background = new Background(bg, 0, 0, QGuiApplication::screens()[0]->availableGeometry().width()*2,
         QGuiApplication::screens()[0]->availableGeometry().height(), this);
     
+    //create obstacles
     firstObstacleX=300;
     distBetweenObstacles=300;
     obstacleGap=250;
@@ -28,11 +41,13 @@ MainWindow::MainWindow(QWidget *parent)
         obstacles.push_back(obBot);
     }
 
+    //run update function every millisecond
     QTimer *timer = new QTimer(this);
     QObject::connect(timer, SIGNAL(timeout()), this, SLOT(update()));
     timer->start(1);
 }
 
+//run every millisecond
 void MainWindow::update(){
     background->update();
     for (unsigned int i = 0; i < obstacles.size()/2; i++) {
