1a2276c901ea0b0706f86ed797686ed01b553b7b
joud lahham <joud@jouds-MacBook-Pro.local>
2024-03-10 19:16:03 -0400

    spaceship & movement

--- headers/mainwindow.h
+++ headers/mainwindow.h
@@ -7,6 +7,8 @@
 #include <vector>
 #include "headers/Background.h"
 #include "headers/Obstacle.h"
+#include "headers/Player.h"
+#include "Player.h"
 
 namespace Ui {
     class MainWindow;
@@ -18,10 +20,18 @@ class MainWindow : public QMainWindow
     Q_OBJECT
 public:
     explicit MainWindow(QWidget *parent = nullptr);
+
+// handle key events
+protected:
+    void keyPressEvent(QKeyEvent *event) override; 
+    void keyReleaseEvent(QKeyEvent *event) override; 
+
 private slots:
     void update();
+
 private:
     Background* background;
+    Player* player;
     std::vector<Obstacle*> obstacles;
     int firstObstacleX;
     int distBetweenObstacles;
--- src/mainwindow.cpp
+++ src/mainwindow.cpp
@@ -1,7 +1,10 @@
 #include "headers/mainwindow.h"
 #include "headers/Background.h"
+#include "headers/Player.h"
 #include <QTimer>
 #include <iostream>
+#include <QGraphicsScene>
+#include <QGraphicsView>
 #include <QGuiApplication>
 #include <QScreen>
 #include <time.h>
@@ -9,7 +12,28 @@
 // the main window of the game
 MainWindow::MainWindow(QWidget *parent)
   : QMainWindow(parent)
-{
+{   
+    // Scene setup
+    QGraphicsScene *scene = new QGraphicsScene(this);
+    QGraphicsView *view = new QGraphicsView(scene, this);
+    
+    // View setup
+    int viewWidth = QGuiApplication::screens()[0]->availableGeometry().width();
+    int viewHeight = QGuiApplication::screens()[0]->availableGeometry().height();
+    view->setSceneRect(0, 0, viewWidth, viewHeight);
+    view->setBackgroundBrush(QBrush(Qt::black));
+
+    QPixmap spaceshipPixmap("img/spaceship.png");
+    int initialX = 100; // X position
+    int initialY = viewHeight / 2; // Y position
+    int spaceshipWidth = 50; // Spaceship width
+    int spaceshipHeight = 30; // Spaceship height
+    
+    player = new Player(spaceshipPixmap, initialX, initialY, spaceshipWidth, spaceshipHeight);
+    scene->addItem(player);
+    player->setFlag(QGraphicsItem::ItemIsFocusable); // Focus on player for key events
+    player->setFocus();
+
     srand(time(NULL));
     QPixmap bg("img/background.png");
     background = new Background(bg, 0, 0, QGuiApplication::screens()[0]->availableGeometry().width()*2,
@@ -31,9 +55,24 @@ MainWindow::MainWindow(QWidget *parent)
     QTimer *timer = new QTimer(this);
     QObject::connect(timer, SIGNAL(timeout()), this, SLOT(update()));
     timer->start(1);
+    setCentralWidget(view);
+    view->show(); 
+}
+
+void MainWindow::keyPressEvent(QKeyEvent *event) {
+    if (player) {
+        player->keyPressEvent(event);
+    }
+}
+
+void MainWindow::keyReleaseEvent(QKeyEvent *event) {
+    if (player) {
+        player->keyReleaseEvent(event);
+    }
 }
 
 void MainWindow::update(){
+    player->update();
     background->update();
     for (unsigned int i = 0; i < obstacles.size()/2; i++) {
         obstacles.at(2*i)->update(0);
