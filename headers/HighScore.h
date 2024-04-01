8b56798cfb9bfb0e5367d2d37f7b80b1b027d5e1
Theo Mulder <tmulder4@uwo.ca>
2024-04-01 19:52:39 -0400

    added doxygen-style comments (hopefully) for highscore files

--- headers/HighScore.h
+++ headers/HighScore.h
@@ -1,11 +1,8 @@
-/**
- * CS 3307
- * Author: Theo Mulder
+/** header that tracks the player's score and highscore
+ *
+ * @author Theo Mulder
  * Last edit: 1 April, 2024
  */
-
-
-// this header is meant to track the player's score, and display it in the top left corner.
 
 #ifndef HIGHSCORE_H
 #define HIGHSCORE_H
--- src/highscore.cpp
+++ src/highscore.cpp
@@ -1,53 +1,93 @@
-/**
- * CS 3307
- * Author: Theo Mulder
+/** file that holds everything to do with scoring
+ *
+ * @author: Theo Mulder
  * Last edit: 1 April, 2024
+ *
+ * implements methods and public/private attributes from highscore.h, and an LCD-type score display
+ * additionally opens a file to keep track of the user's highscore across various games
  */
 
-// header file
+/// header files
 #include "headers/HighScore.h"
 #include <QFile>
 #include <QString>
-//#include <iostream>
+
+/// defines
+#define DIGITS_IN_DISPLAY 3
 
 
-// initialize the score display with 0 in score and highscore
-HighScore::HighScore(QWidget *parent) : QLCDNumber(3, parent) {
+
+/**
+ * @brief   score/highscore constructor
+ *
+ * constructor is used to set up and show the player's score in the game
+ * uses a QLCDNumber object to display the score in a more space-y theme
+ *
+ * @param *parent   pointer for the parent
+ * @param DIGITS_IN_DISPLAY    How many digits can be shown on the score's LCD display
+ * @param parent    parent window for the QLCDNumber display to attach to
+ */
+HighScore::HighScore(QWidget *parent) : QLCDNumber(DIGITS_IN_DISPLAY, parent) {
     score = 0;
     highScore = 0;
 
-    QFile HSFile ("highscore.txt");
+    QFile HSFile ("highscore.txt"); /*! the file that holds/saves the highscore */
     HSFile.open(QIODevice::ReadWrite);
     if (!HSFile.atEnd()){
-        // if there's a previous highscore, update the score function
+        /// if there's a previous highscore from the file, update the score function
         QString highscoreST = HSFile.readLine();
         highScore = highscoreST.toInt();
     }
     HSFile.close();
 
-    resize(150, 100);
-    display(score);
+
+    resize(150, 100); /// modifies the LCD display in the top left of the window
+    display(score);   /// and updates it to display the current score (0)
 
 }
 
-// getter functions for score and highscore
+/**
+ * @brief   score getter
+ * getter function to return the player's current score
+ *
+ * @return score (int)
+ */
 int HighScore::getScore(){
     return score;
 }
 
+/**
+ * @brief   highscore getter
+ * getter function to return the player's highscore
+ * can be less than the current score, if it hasn't been updated yet.
+ *
+ * @return highscore (int)
+ */
 int HighScore::getHighScore(){
     return highScore;
 }
 
+/**
+ * @brief   score setter
+ * setter function to change the player's current score
+ * @param newScore  the new current score
+ */
 // setter function for the score
 void HighScore::updateScore(int newScore){
     score = newScore;
     display(score);
 }
 
-// pseudo-setter function for the highscore
-// (technically not a setter, as it doesn't accept any external values)
-// intended to be used after/during the 'Game Over' state
+/**
+ * @brief   highscore checker
+ * checks if the score is greater than the highscore,
+ * and then updates the highscore var and highscore file to match.
+ *
+ * technically the highscore's setter method, as it's the only way to update it.
+ * intended to be used after dying to an obstacle
+ *
+ * @return  if the player's score is greater than the highscore
+ */
 bool HighScore::checkNewHighScore(){
     if (score > highScore){
         highScore = score; // update the highScore var
@@ -62,7 +102,11 @@ bool HighScore::checkNewHighScore(){
     return false;
 }
 
-// new run, set/initialize score to 0
+/**
+ * @brief   score resetter
+ * resetter function to change the player's to zero
+ * so that they can start a new run
+ */
 void HighScore::resetScore(){
     score = 0;
     display(score);
